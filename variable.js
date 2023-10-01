6eef020cf120694daf9ccecb88da11c03e4fb5ea
AjitpawarA <pawarac18@gmail.com>
2023-10-01 14:41:55 +0530

    Added first file of js

---
+++ variable.js
@@ -0,0 +1,23 @@
+// Variables in java script
+
+let name ="Ajit Pawar";
+console.log(name);
+
+var age = 10;
+console.log(age);
+
+const mob = 95555;
+console.log(mob);
+
+
+name = "Nikhil";
+age = 15;
+
+console.log(name);
+console.log(age);
+
+
+// Not Allowed 
+// mob=2154;
+// console.log(mob);
+
