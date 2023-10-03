888e37b5aee1fa2373f095c4ec801f3b921aebbc
AjitpawarA <pawarac18@gmail.com>
2023-10-04 01:30:11 +0530

    Added some basic code for Functions

---
+++ 19_functions.js
@@ -0,0 +1,27 @@
+//Functions in java 
+
+ function saymyname(){
+    console.log("Ajit");
+ }
+
+ saymyname();
+ saymyname();
+
+
+ function addition(num1, num2){
+    console.log("Addition = "+ (num1+num2));
+ }
+
+addition(10,20);
+
+
+
+function substraction(no1,no2){
+    let result = no1-no2;
+    return result;
+}
+console.log(substraction(30,20));
+
+
+let result = substraction(30,20);
+console.log("Result = "+ result);
